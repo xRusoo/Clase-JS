3db23a955eccddbe505cf9d20fbefdf25dc84bed
Walter <walterf952@gmail.com>
2023-05-09 02:33:53 -0300

    Ejercicio1Terminado

---
+++ Tarea JS 1/ejercicio1.js
@@ -0,0 +1,9 @@
+function imprimirNumeros(numero){
+    let acum="";
+    for (let index = 0; index < numero; index++) {
+        acum = acum+(index+1);
+        console.log(acum);
+    }
+}
+
+imprimirNumeros(10);
