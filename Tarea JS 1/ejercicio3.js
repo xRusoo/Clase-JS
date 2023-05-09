ab23c51c8288cdaec2e6a4329a3106e71a80dd11
Walter <walterf952@gmail.com>
2023-05-09 03:00:37 -0300

    Identar codigo

--- Tarea JS 1/ejercicio3.js
+++ Tarea JS 1/ejercicio3.js
@@ -1,31 +1,31 @@
-//objeto a respetar 
+//objeto a respetar
 let carrito = {
-    montoTotal: 10,
-    productos: ["Leche"]
-}
+  montoTotal: 10,
+  productos: ["Leche"],
+};
 //Clase creada
 class Carrito {
-    montoTotal;
-    productos;
+  montoTotal;
+  productos;
 
+  constructor(montoInicial, productoInicial) {
+    this.montoTotal = montoInicial;
+    this.productos = productoInicial;
+  }
 
-    constructor(montoInicial, productoInicial){
-        this.montoTotal = montoInicial;
-        this.productos = productoInicial;
+  agregarProducto(nombre, precio, unidades) {
+    if (this.productos.includes(nombre)) {
+    } else {
+      this.productos.push(nombre);
+      this.montoTotal = this.montoTotal + precio * unidades;
     }
-
-    agregarProducto(nombre, precio, unidades) {
-        this.productos.push(nombre);
-        this.montoTotal = this.montoTotal +(precio*unidades);
-    }
-
+  }
 }
-
 
 let CarritoPrueba = new Carrito(carrito.montoTotal, carrito.productos);
 
 console.log(CarritoPrueba);
 
-CarritoPrueba.agregarProducto("Yerba",15,2);
+CarritoPrueba.agregarProducto("Yerba", 15, 2);
 
 console.log(CarritoPrueba);
